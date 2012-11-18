efcb80364ed1370faf21a659f9d130d644ab5714
Sonicrida <Sonicrida@live.com>
2012-11-18 04:18:40 -0500

    registering should work with proper formatting

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -21,11 +21,28 @@ class UserTest < ActiveSupport::TestCase
 
   test "a user should have a unique profile name" do
   	user = User.new
-	user.profile_name = users(:jonathan).profile_name
+	  user.profile_name = users(:jonathan).profile_name
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   end
 
+  test "a user should have a profile name with no spaces" do
+    user = User.new(first_name: 'Jonathan', last_name: 'Chhabra', email: 'Sonicrida2@gmail.com')
+    user.password = user.password_confirmation = 'asaasas'
+    user.profile_name = "My Profile With Spaces"
+
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted name" do
+    user = User.new(first_name: 'Jonathan', last_name: 'Chhabra', email: 'Sonicrida2@gmail.com')
+    user.password = user.password_confirmation = 'asaasas'
+
+    user.profile_name = 'legitusername'
+    assert user.valid?
+  end
 
 end
