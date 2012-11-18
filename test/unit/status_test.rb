79722064c5c3428008f942f9349fad2269b0471b
Sonicrida <Sonicrida@live.com>
2012-11-18 04:03:37 -0500

    added validations for statuses

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -8,4 +8,18 @@ class StatusTest < ActiveSupport::TestCase
   	assert !status.save
   	assert !status.errors[:content].empty?
   end
+
+  test "that a status' content is at least 2 letters long" do
+  	status = Status.new
+  	status.content = "H"
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user id" do
+  	status = Status.new
+  	status.content = "Hello!"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
 end
